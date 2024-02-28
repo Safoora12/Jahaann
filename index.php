4f1a61b3ad79a43d0ee1fb6e2f0155d8a69033b2
Safoora Iftikhar <developer_web2@yahoo.com>
2024-02-28 16:16:23 +0500

    index changed

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-
-<?php
-    include ('/pages/areal_view.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jahaann</title>
+</head>
+<body>
+    <h1>THIS IS THE MAIN PAGE<h1>
+</body>
+</html>
